d390bd94a9de5ee6862b3ab20c6ad4e91c80f66f
Samuel Edem Jerry-Kanfra <edemkweku.jerrykanfra@gmail.com>
2025-06-02 23:41:29 +0000

    first fixes on master

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:gag_cars_frontend/Pages/Authentication/Screens/forgotPasswordPage.dart';
 import 'package:gag_cars_frontend/Pages/Authentication/Screens/resetPasswordPage.dart';
 import 'package:gag_cars_frontend/Pages/Authentication/Screens/signInWithPhonePage.dart';
